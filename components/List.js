12ce5b460e7a1cb06d8c4b4471de1ee7460854f6
Kevin GANEM <Ganemkevin@gmail.com>
2022-03-01 14:55:48 +0100

    too many requests from api, they blocked me ATM

--- components/List.js
+++ components/List.js
@@ -1,17 +1,36 @@
+// --------------------------------------------------------------------------- //
+// --------------------------------------------------------------------------- //
+// --------------------------------------------------------------------------- //
+// --------------------------      LIST.JS     ------------------------------- //
+// --------------------------------------------------------------------------- //
+// --------------------------------------------------------------------------- //
+// --------------------------------------------------------------------------- //
+
 // REACT
 import { useEffect, useState } from "react";
 // REACT-NATIVE
-import { FlatList, Text, ScrollView, StyleSheet, View } from "react-native";
+import {
+  FlatList,
+  Text,
+  ScrollView,
+  StyleSheet,
+  View,
+  ActivityIndicator,
+  Pressable,
+} from "react-native";
 import { Card } from "react-native-elements";
 
 export default function List() {
   const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [weather, setWeather] = useState([]);
 
   useEffect(() => {
     fetch(`https://restcountries.com/v3.1/all`)
       .then((res) => res.json())
       .then((res) => {
         setCountries(res);
+        setLoading(false);
         console.log(setCountries);
       })
       .catch((err) => console.log(err));
@@ -24,6 +43,7 @@ export default function List() {
           <Card.Title>{item.name.common}</Card.Title>
           <Card.Divider />
           <Card.Image style={styles.img} source={{ uri: item.flags.png }} />
+
           <Text style={styles.text}>{item.capital}</Text>
         </Card>
       </View>
@@ -31,6 +51,7 @@ export default function List() {
   };
   return (
     <ScrollView>
+      {countries.length === 0 && loading && <ActivityIndicator size="large" />}
       <FlatList
         data={countries}
         renderItem={renderCountry}
